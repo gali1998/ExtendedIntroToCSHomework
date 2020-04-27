8943375c0b0f11ee1dbcdf8e80810ce2c0a94c1e
Unknown <gali1998@gmail.com>
2020-04-27 15:40:43 +0300

    actual tests

--- 3/testhelper.py
+++ 3/testhelper.py
@@ -5,3 +5,26 @@ def get_random_string(k):
     letters = ["a", "b", "c", "d"]
 
     return rand_string.join(random.choice(letters) for i in range(k))
+
+def rotate(L, k):
+    n = len(L)
+    return L[k%n:] + L[:k%n]
+
+def create_random_list():
+    n = random.randrange(1, 100)
+
+    lst = [random.randrange(-100, 100) for i in range(n)]
+
+    return lst
+
+def create_random_sorted_list():
+    lst = create_random_list()
+    lst.sort()
+
+    return lst
+
+def get_random_rotated_list():
+    k = random.randrange(1, 500)
+    lst = create_random_sorted_list()
+
+    return rotate(lst, k)
---
+++ 3/tests.py
@@ -0,0 +1,119 @@
+import unittest
+from skeleton import *
+from testhelper import *
+
+class TestQ3(unittest.TestCase):
+    def test_random_100(self):
+        result = True
+
+        for i in range(100):
+            lst = create_random_list()
+            copy = lst.copy()
+            copy.sort()
+            if len(lst) != 1:
+                k = random.randrange(1, len(lst))
+            else:
+                k = 1
+
+            if sort_by_block_merge(lst, k) != copy:
+                result = False
+                break
+
+        self.assertTrue(result)
+
+    def test_random(self):
+        lst = create_random_list()
+        copy = lst.copy()
+        copy.sort()
+        if len(lst) != 1:
+            k = random.randrange(1, len(lst))
+        else:
+            k = 1
+        self.assertEqual(sort_by_block_merge(lst, k), copy)
+
+class TestQ4(unittest.TestCase):
+    def test_find_random(self):
+        lst = get_random_rotated_list()
+
+        s = random.choice(lst)
+
+        self.assertEqual(s, lst[find(lst, s)])
+    def test_find_missing_random(self):
+        n = random.randrange(0, 10000)
+        k = random.randrange(0, n + 1)
+
+        lst = [i for i in range(0, n+1) if i != k]
+
+        self.assertEqual(find_missing(lst, k), k)
+
+    def test_find_missing_random100(self):
+        result = True
+
+        for i in range(100):
+            n = random.randrange(0, 10000)
+            k = random.randrange(0, n + 1)
+
+            lst = [i for i in range(0, n + 1) if i != k]
+
+            if find_missing(lst, k) != k:
+                result = False
+
+                break
+
+        self.assertTrue(result)
+
+class TestQ5(unittest.TestCase):
+    def test_sort(self):
+        n = random.randrange(1, 30)
+        k = random.randrange(1, 10)
+
+        lst = [get_random_string(k) for i in range(n)]
+        lst1 = lst.copy()
+        lst2 = lst.copy()
+        lst3 = lst.copy()
+        lst1.sort()
+
+        self.assertEqual(sort_strings1(lst2, k), lst1)
+        self.assertEqual(sort_strings2(lst3, k), lst1)
+
+    def test_int_to_string_random(self):
+        k = random.randrange(1, 30)
+        n = random.randrange(0, 5**k)
+
+        self.assertEqual(string_to_int(int_to_string(k, n)), n)
+
+    def test_int_to_string_for_all_values(self):
+        k = random.randrange(1, 10)
+
+        result = True
+
+        for i in range(0, 5**k):
+            if string_to_int(int_to_string(k, i)) != i:
+                result = False
+                break
+
+        self.assertTrue(result)
+
+    def test_string_to_int_random(self):
+        k = random.randrange(1, 30)
+        str1 = get_random_string(k)
+        str2 = get_random_string(k)
+
+        self.assertEqual(string_to_int(str1) >= string_to_int(str2), str1 >= str2)
+
+    def test_string_to_int_random100(self):
+        result = True
+
+        for i in range(100):
+            k = random.randrange(1, 30)
+            str1 = get_random_string(k)
+            str2 = get_random_string(k)
+            val1 = string_to_int(str1)>= string_to_int(str2)
+            val2 = str1 >= str2
+
+            if  val1 != val2:
+                print(str1+ " " + str2)
+                result = False
+
+                break
+        self.assertTrue(result)
